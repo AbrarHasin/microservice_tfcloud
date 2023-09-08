8b1c7b1542f10082a36bd6ee0c5833f152a13bc5
abrarbjit <abrar.hasin@bjitgroup.com>
2023-09-08 15:43:13 +0600

    Initial commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+# Terraform Settings Block
+terraform {
+  required_version = ">= 1.5.7"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.16.0"
+    }
+  }
+}
+
+provider "aws" {
+  #region = "ap-northeast-1"
+}
