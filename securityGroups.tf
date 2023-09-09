2cf2a89002b0c7652efdb639dd8b9ab1b7ebcb22
AbrarHasin <abrarhasinpranto@gmail.com>
2023-09-09 21:26:12 +0600

    ALB added

---
+++ securityGroups.tf
@@ -0,0 +1,16 @@
+module "alb_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 5.0"
+
+  name        = "${local.name}-service"
+  description = "Service security group"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_rules       = ["http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = module.vpc.private_subnets_cidr_blocks
+
+  tags = local.tags
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 locals {
   region = "ap-southeast-1"
-  name   = "ex-${basename(path.cwd)}"
+  #   name   = "ex-${basename(path.cwd)}"
+  name = "microservice"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
@@ -9,8 +10,8 @@ locals {
   container_port = 80
 
   tags = {
-    Name       = local.name
-    Example    = local.name
+    Name    = local.name
+    Example = local.name
     #Repository = "https://github.com/terraform-aws-modules/terraform-aws-ecs"
   }
 }
