abe1625dfb5e40f0d5051d090cada23d8dc33ce1
AbrarHasin <abrarhasinpranto@gmail.com>
2023-09-09 20:38:50 +0600

    VPC Module created

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Terraform Settings Block
 terraform {
-  #required_version = ">= 1.5.7"
+  # required_version = ">= 1.5.7"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -10,5 +10,5 @@ terraform {
 }
 
 provider "aws" {
-  #region = "ap-northeast-1"
+  # region = "ap-northeast-1"
 }
